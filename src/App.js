95101317ce3786d8f9b7ae3880c62723c7bbedee
LeedWover <aresone.gaben@gmail.com>
2020-06-15 20:13:48 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [rate, setRate] = useState(35)
+  const [petrol, setPetrol] = useState(30)
+  const [result, setResult] = useState(0)
+
+  useEffect(() => {
+    rateCounter()
+  }, []);
+
+  const rateCounter = () => {
+    setResult(petrol / (rate / 1000))
+  }
+
+  const handleRate = (event) => {
+    setRate(event.target.value)
+  }
+
+  const handlePetrol = (event) => {
+    setPetrol(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Arány: 1:<input type="number" value={rate} onChange={handleRate}/>
+      </div>
+      <div>
+        Üzemanyag: <input type="number" value={petrol} onChange={handlePetrol}/> liter
+      </div>
+      <button onClick={rateCounter}>
+        Kiszámol
+      </button>
+      <div>
+        {Math.round(result)} ml
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
